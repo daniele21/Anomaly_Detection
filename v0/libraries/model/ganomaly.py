ea72c4d938b2a15c390361e8f6e70b28c63942c2
daniele21 <daniele.moltisanti@mail.polimi.it>
2020-01-03 11:35:43 +0100

    filters v0

--- v0/libraries/model/ganomaly.py
+++ v0/libraries/model/ganomaly.py
@@ -20,8 +20,8 @@ from libraries.utils import EarlyStopping, saveInfoGanomaly, addInfoGanomaly, LR
 from libraries.utils import Paths, ensure_folder, getNmeans
 from libraries.dataset_package.dataset_manager import generatePatches
 
-from scipy.ndimage import convolve
-from scipy.signal import medfilt
+from scipy.ndimage import convolve, median_filter
+#from scipy.signal import medfilt
 
 paths = Paths()
 
@@ -362,13 +362,13 @@ class AnomalyDetectionModel():
 
             # CONV ANOMALY SCORE
             kernel = torch.ones(size=(len(self.validationloader.dataset),), dtype=torch.float32, device=device)
-            kernel = kernel/len(self.validationloader)
+            #kernel = kernel/len(self.validationloader)
             conv_anom_scores = convolve(anomaly_scores.cpu(), kernel.cpu())
             auc_conv, conv_threshold = evaluate(gt_labels, conv_anom_scores, plot=True,
                                                 folder_save=self.folder_save, info='conv')
 
             # MEDIAN ANOMALY SCORE
-            median_anom_scores = medfilt(anomaly_scores.cpu())
+            median_anom_scores = median_filter(anomaly_scores.cpu())
             auc_median, median_threshold = evaluate(gt_labels, median_anom_scores, info='median',
                                                     plot=True, folder_save=self.folder_save)
 
--- v0/libraries/model/postprocessing.py
+++ v0/libraries/model/postprocessing.py
@@ -3,8 +3,14 @@
 #%%
 import torch
 import numpy as np
-from scipy.ndimage import convolve
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from libraries.dataset_package.dataset_manager import computeMask, Image
+
+from scipy.ndimage import convolve, median_filter
 from scipy.signal import medfilt
+import cv2
 #%% CONVOLUTION
 
 anom = np.array([[0, 1.5, 2.3],
@@ -18,3 +24,44 @@ kernel
 
 convolve(anom, kernel)
 medfilt(anom)
+#median_filter(anom, kernel)
+
+#%%
+train = pd.read_csv('libraries/dataset_package/train_unique.csv', index_col=0)
+i = 0
+#%%
+i += 1
+#%%
+enc = train.iloc[i].Encoded_Pixels
+filename = train.iloc[i].Image_Id
+
+img = Image(filename)
+mask = computeMask(enc, img)
+
+plt.imshow(mask)
+
+#%%
+#k = np.ones((256,1600), dtype='uint8')
+med = median_filter(mask, size=(32,4))
+plt.show(med)
+plt.show()
+
+kernel = np.ones((256,1600)) / (256*1600) 
+conv = convolve(mask, kernel)
+plt.show(conv)
+plt.show()
+#%%
+med = cv2.medianBlur(mask, ksize=21)
+print(len(np.where(med)[0]))
+print(len(np.where(mask)[0]))
+
+gauss = cv2.GaussianBlur(med, ksize=7)
+print(len(np.where(gauss)[0]))
+
+plt.imshow(med)
+plt.show()
+plt.imshow(mask)
+plt.show()
+plt.imshow(gauss)
+plt.show()
+
