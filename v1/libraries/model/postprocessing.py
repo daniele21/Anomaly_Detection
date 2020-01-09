feb83c1a9d64bb5aedf08bde9a8704de6e479517
daniele21 <daniele.moltisanti@mail.polimi.it>
2020-01-09 10:52:17 +0100

    postprocessing distscores

--- v1/libraries/model/postprocessing.py
+++ v1/libraries/model/postprocessing.py
@@ -172,8 +172,8 @@ def distScores(anomaly_scores, gt_labels, threshold, figsize=(7,5)):
     
     # DISTRIBUTIONS 
     
-    sn.distplot(anomalies, bins=100, norm_hist=True, label='Anomaly Score')
-    sn.distplot(normals, bins=100, norm_hist=True, label='Normal Score')
+    sn.distplot(anomalies, bins=1000, norm_hist=True, label='Anomaly Score')
+    sn.distplot(normals, bins=1000, norm_hist=True, label='Normal Score')
 
     plt.xlim(0, x_limit)
     plt.legend()
