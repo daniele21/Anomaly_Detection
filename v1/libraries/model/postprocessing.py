9b9e94b8c5b1da9582cda939cd1873127fc7a493
daniele21 <daniele.moltisanti@mail.polimi.it>
2020-01-09 10:56:08 +0100

    postprocessing distscores

--- v1/libraries/model/postprocessing.py
+++ v1/libraries/model/postprocessing.py
@@ -155,7 +155,7 @@ def distScores(anomaly_scores, gt_labels, threshold, figsize=(7,5)):
     plt.figure(figsize=figsize)
     x_limit = np.mean(anomalies)
     
-    values, _, _ = plt.hist([anomalies, normals], bins=100,
+    values, _, _ = plt.hist([anomalies, normals], bins=1000,
              label=['Anomaly Scores', 'Normal Scores'], density=True)
 #    return values
     x1, y1 = [threshold, threshold], [0,max(values[1])]
@@ -172,8 +172,11 @@ def distScores(anomaly_scores, gt_labels, threshold, figsize=(7,5)):
     
     # DISTRIBUTIONS 
     
-    sn.distplot(anomalies, bins=1000, norm_hist=True, label='Anomaly Score')
-    sn.distplot(normals, bins=1000, norm_hist=True, label='Normal Score')
+#    sn.distplot(anomalies, bins=1000, norm_hist=True, label='Anomaly Score')
+#    sn.distplot(normals, bins=1000, norm_hist=True, label='Normal Score')
+    
+    sn.distplot(anomalies, bins=1000, hist=False, label='Anomaly Score')
+    sn.distplot(normals, bins=1000, hist=False, label='Normal Score')
 
     plt.xlim(0, x_limit)
     plt.legend()
