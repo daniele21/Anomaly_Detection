2a767f368fe8bf35f17089f5e6a4feb79515f115
daniele21 <daniele.moltisanti@mail.polimi.it>
2020-01-10 13:38:39 +0100

    base version

--- base/libraries/tests/test_result.py
+++ base/libraries/tests/test_result.py
@@ -118,7 +118,8 @@ def automaticEvaluation(model, samples, stride, folder_save=None):
     train = pd.read_csv(csv_directory + 'train_unique.csv', index_col=0)
     
     for index in samples:        
-        img, _, mask = dm.extractPatchesForTest(train, index, shape, stride, model_name)
+        img, _, mask = dm.extractPatchesForTest(train, index, shape, stride,
+                                                model_name, folder_save)
         evaluateResult(model, img, mask, stride, folder_save)
         
 
